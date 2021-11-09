e09bba124d75130167b7f730a8c32301605c4878
Deadpool27 <44410726+Deadpool2701@users.noreply.github.com>
2021-11-09 21:00:20 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyADWwHlQvFDLtlA_ruI06B-HgK8K_JGa8U",
-  authDomain: "text3-e575c.firebaseapp.com",
-  projectId: "text3-e575c",
-  storageBucket: "text3-e575c.appspot.com",
-  messagingSenderId: "397405219117",
-  appId: "1:397405219117:web:76cc1943ba64f5aabccba4",
-  measurementId: "G-R5ZTZB79RL"
+  apiKey: 
+  authDomain: 
+  projectId:
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
